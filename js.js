a4e176dd2a531016a721bf79f7febadaf4c2b1ec
ismailbgr <ozismailbugra@gmail.com>
2017-12-02 13:08:23 +0300

     

---
+++ js.js
@@ -0,0 +1,33 @@
+var font = 1;
+
+function changefont(x){
+	document.getElementById("txtinput").classList.remove("font"+font)
+	document.getElementById("fontselect").classList.remove("font"+font)
+	document.getElementById("txtinput").classList.add("font"+x)
+	document.getElementById("fontselect").classList.add("font"+x)
+	font = x
+}
+
+function Showcreate(){
+	$("#settxt").slideDown(1000);
+	$("#createbutton").hide();
+}
+
+function create(){
+	location.hash = font + window.btoa(document.getElementById("txtinput").value)
+	location.reload();
+	}
+
+window.onload = function(){
+if(location.hash != ""){
+var messagebox = document.getElementById("txt")
+var hash = location.hash;
+var realhash = hash.substring(2)
+var fontinhash = hash.substring(1,2)
+messagebox.classList.add("font"+fontinhash);
+messagebox.innerHTML = window.atob(realhash)
+document.getElementById("url").value = location.href;
+$("#txtbox").slideDown(1000);
+$("#createbutton").hide();
+}
+}
